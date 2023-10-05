a02066bdf79cf2eff23cc5d09e943e798d4b8c13
Erin Santosa <tun35949@temple.edu>
2023-10-05 12:12:40 -0400

    passed selected value back to activity that launched TextSizeActivity

--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,7 @@
 package edu.temple.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,14 +25,14 @@ class TextSizeActivity : AppCompatActivity() {
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
-            adapter = TextSizeAdapter(textSizes){
-
+            adapter = TextSizeAdapter(textSizes){ selectedTextSize ->
+                val resultIntent = Intent()
+                resultIntent.putExtra("selectedTextSize", selectedTextSize)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
-
-
-
     }
 }
 
@@ -60,11 +62,3 @@ class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: 
     }
 
 }
-
-
-
-
-
-
-
-
