8ad8d2797fbf0fb44f8f17c414eccd2c13d70e86
Erin Santosa <tun35949@temple.edu>
2023-10-05 12:09:23 -0400

    created launcher & called launcher when button clicked (still buggy)

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,13 @@
 package edu.temple.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.RecyclerView
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -14,12 +18,21 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val selectedTextSize = findViewById<RecyclerView>(R.id.textSizeSelectorRecyclerView).getIntExtra("selectedTextSize", 0)
+        lyricsDisplayTextView.textSize = selectedTextSize.toFloat()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
-        textSizeSelectorButton = findViewById<Button?>(R.id.textSizeSelectorButton)
+        textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
 
+        textSizeSelectorButton.setOnClickListener(){
+            val intent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+            launcher.launch(intent)
+        }
     }
 }
